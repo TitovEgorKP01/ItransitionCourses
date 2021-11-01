e01c02cbceec847941ffd3f63621b0d696601ef5
Egor Titov <tegor901@gmail.com>
2021-11-01 19:13:29 +0200

    Task_3 completed

---
+++ Tasks/Task_3/GameRules.cs
@@ -0,0 +1,27 @@
+namespace Task_3
+{
+    static class GameRules
+    {
+        public static int GetWinner(int indexSide1, int indexSide2, int arrayLength)
+        {
+            int diff = indexSide1 - indexSide2;
+
+            int halfLength = arrayLength / 2;
+            
+            if (diff == 0)
+            {
+                return -1;
+            }
+            else if (diff < 0)
+            {
+                if (-diff <= halfLength) return indexSide2; 
+                else return indexSide1;
+            }
+            else
+            {
+                if (diff <= halfLength) return indexSide1;
+                else return indexSide2;
+            }
+        }
+    }
+}
---
+++ Tasks/Task_3/HMACGenerator.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Task_3
+{
+    static class HMACGenerator
+    {
+        public static byte[] GenerateRandomKey(int size)
+        {
+            using (var generator = RandomNumberGenerator.Create())
+            {
+                var salt = new byte[size];
+                generator.GetBytes(salt);
+                return salt;
+            }
+        }
+
+        public static byte[] HashHMAC(byte[] key, byte[] message)
+        {
+            var hash = new HMACSHA256(key);
+            return hash.ComputeHash(message);
+        }
+
+        public static byte[] StringEncode(string text)
+        {
+            var encoding = new ASCIIEncoding();
+            return encoding.GetBytes(text);
+        }
+
+        public static string HashEncode(byte[] hash)
+        {
+            return BitConverter.ToString(hash).Replace("-", "").ToLower();
+        }
+    }
+}
